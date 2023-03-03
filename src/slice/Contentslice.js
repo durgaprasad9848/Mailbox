7069385f52250320bd1f27a3d2afb4324b329bf9
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2023-03-03 09:46:40 +0530

    deletemail

--- src/slice/Contentslice.js
+++ src/slice/Contentslice.js
@@ -1,5 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+ 
+ 
  const Contentslice = createSlice({
     name : "content",
     initialState: {
@@ -11,13 +14,16 @@ import { createSlice } from "@reduxjs/toolkit";
         storeindbox : (state,action)=>{
             state.indboxdata = action.payload;
             console.log("inside red",state.indboxdata);
+            state.totalUnread=0;
+            Object.keys(state.indboxdata).map((key) => { if(!state.indboxdata[key].isVisited){ state.totalUnread++;  } });
 
         },
         storesentbox : (state,action)=>{
             state.sentboxdata = action.payload;
         },
-        updateCount : (state,action)=>{
-            state.totalUnread = action.payload;
+        updateCount : (state)=>{
+            state.totalUnread=0;
+            Object.keys(state.indboxdata).map((key) => { if(!state.indboxdata[key].isVisited){ state.totalUnread++;  } });
         }
     }
  })
@@ -25,3 +31,23 @@ import { createSlice } from "@reduxjs/toolkit";
  export const{storeindbox,storesentbox,updateCount} = Contentslice.actions;
 
  export default Contentslice.reducer;
+
+export const fetchDataind = async (dispatch) => {
+  
+   // const dispatch = useDispatch();
+    try {
+      const response = await axios.get(
+        `https://test-api-c7d27-default-rtdb.firebaseio.com/${localStorage.getItem("email").replace("@gmail.com", "")}/receive.json`
+      );
+      if (response.data != null) {
+        dispatch(storeindbox(response.data));
+        //countfun(dispatch);
+        //Object.keys(inboxdata).map((key) => { if(!inboxdata[key].isVisited){ count++;  } });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+ 
+  
